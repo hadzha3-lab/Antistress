fb901300711d7defd21cd994a03aca125a2773a6
hadzha3-lab <hadzha3@gmail.com>
2026-09-26 01:26:10 +0300

    Add app/src/main/java/lab/hadzha3/antistress/MainActivity.kt

---
+++ app/src/main/java/lab/hadzha3/antistress/MainActivity.kt
@@ -0,0 +1,13 @@
+package lab.hadzha3.antistress
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.WindowManager
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        setContentView(AntiStressView(this))
+    }
+}
